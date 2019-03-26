d5a8f3737d012d589c1d3294c5ff7a933ba5a9ee
Jeff Escalante <gh.je@mailhero.io>
2019-03-26 13:55:14 -0400

    touchups

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withSize = require('next-size')
+
+module.exports = withSize({
   pageExtensions: ['js', 'jsx', 'mdx'],
   webpack(config, options) {
     config.module.rules.push({
@@ -7,4 +9,4 @@ module.exports = {
     })
     return config
   }
-}
+})
