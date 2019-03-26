c3de8c8b3a6fe418feb54288583502ee9558ad7b
Jeff Escalante <gh.je@mailhero.io>
2019-03-26 13:39:20 -0400

    minimal reproduction of tree shaking issue

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-const {
-  withHashicorp,
-  defineStaticRoutes
-} = require('@hashicorp/next-hashicorp')
-const routes = require('./routes')
-const path = require('path')
-
-module.exports = withHashicorp({
-  async exportPathMap() {
-    return defineStaticRoutes(routes)
-  },
-
-  webpack(config) {
+module.exports = {
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  webpack(config, options) {
     config.module.rules.push({
-      test: /\.yml$/,
-      use: [
-        { loader: path.join(__dirname, 'node_modules/json-loader') },
-        { loader: path.join(__dirname, 'node_modules/yaml-loader') }
-      ]
+      test: /\.mdx$/,
+      use: [options.defaultLoaders.babel, '@mdx-js/loader']
     })
     return config
   }
-})
+}
--- routes.js
+++
@@ -1,6 +0,0 @@
-const defineDynamicRoutes = require('@hashicorp/next-hashicorp/dist/routes')
-
-const routes = []
-
-module.exports = routes
-module.exports.Link = defineDynamicRoutes(routes).Link
